88d7b1037ae9cf817c9fafd9a9016aca79c3fced
Richard Westberg <westberg.richard@gmail.com>
2021-03-28 20:36:59 +0200

    order imports

--- pages/writing/evolution-part-ii.js
+++ pages/writing/evolution-part-ii.js
@@ -1,5 +1,5 @@
-import Layout from '../../components/layout'
 import Image from 'next/image'
+import Layout from '../../components/layout'
 
 export const title = 'Evolution Part II: Gaggia Evolution'
 
