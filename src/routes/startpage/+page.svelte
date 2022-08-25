30787b592a1f6c084b592557debcdf83145de9f9
Richard Westberg <westberg.richard@gmail.com>
2022-08-25 07:28:15 +0200

    wip: personal startpage

---
+++ src/routes/startpage/+page.svelte
@@ -0,0 +1,31 @@
+<h1>Startpage</h1>
+
+<ul class="list">
+	<li><a href="https://news.ycombinator.com">hn</a></li>
+	<li><a href="https://svt.se">svt</a></li>
+	<li><a href="https://gp.se">gp</a></li>
+	<li><a href="https://hltv.org">hltv</a></li>
+	<div class="spacer" />
+	<li><a href="https://www.reddit.com/r/espresso/">reddit espresso</a></li>
+	<li><a href="https://www.reddit.com/r/sweden/">reddit sweden</a></li>
+	<li><a href="https://www.reddit.com/r/GlobalOffensive/">reddit csgo</a></li>
+	<div class="spacer" />
+	<li><a href="https://www.facebook.com/marketplace/">fb marketplace</a></li>
+	<li>
+		<a href="https://www.blocket.se/annonser/goteborg/fritid_hobby/cyklar?cg=6060&f=p&r=15&ag=1"
+			>blocket bikes</a
+		>
+	</li>
+	<li><a href="https://www.blocket.se/annonser/hela_sverige?q=espresso">blocket espresso</a></li>
+	<li>
+		<a href="https://www.blocket.se/annonser/hela_sverige?q=espressomaskin"
+			>blocket espressomaskin</a
+		>
+	</li>
+</ul>
+
+<style>
+	.spacer {
+		margin-top: 0.5rem;
+	}
+</style>
