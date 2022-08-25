a6f18c6b1fff5451f1afc3c96240694a7fa78f94
Richard Westberg <westberg.richard@gmail.com>
2022-08-25 07:26:23 +0200

    add boilerplate for writing

---
+++ src/routes/writing/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Writing</h1>
+
+<ul>
+	<li>
+		<a href="https://github.com/wric">Evolution Part I: Introduction</a>
+	</li>
+	<li>
+		<a href="mailto:westberg.richard@gmail.com">Evolution Part II: Gaggia Evolution</a>
+	</li>
+</ul>
