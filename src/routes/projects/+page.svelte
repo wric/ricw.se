5348d9e648fd36af178e902dfc5f13a52f0fdb54
Richard Westberg <westberg.richard@gmail.com>
2022-08-25 07:27:32 +0200

    draf of projects page

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,47 @@
+<h1>Projects</h1>
+
+<h3>Website</h3>
+<p>
+	The current itteration of this website is written with SvelteKit in mid 2022. It's resembling the
+	previous one (written in NextJS), but with a bit cleaner and less styled. At my current employee,
+	we work a lot with NextJS and I wanted to try Svelte out of curiosity.
+</p>
+<ul>
+	<li><a href="https://ricw.se">See it live</a></li>
+	<li><a href="https://github.com/wric/ricw.se">Source code on Github</a></li>
+</ul>
+
+<h3>Tyler</h3>
+<p>
+	Tyler is a small utility site for previewing patterns from
+	<a href="https://www.toptal.com/designers/subtlepatterns/">Subtle Patterns</a>. It's writting in
+	pure JavaScript and using <a href="https://tachyons.io/">Tachyons</a> for stying. I was curious about
+	utility based CSS framworks and want the creat the little tool for myself without the need of huge
+	CSS and JavaScript frameworks.
+</p>
+<ul>
+	<li><a href="https://tyler.ricw.se">See it live</a></li>
+	<li><a href="https://github.com/wric/tyler">Source code on Github</a></li>
+</ul>
+
+<h3>Is bolaget closed</h3>
+<p>
+	You know, I live in Sweden where we have the alcohol monopoly owned by the government. Without
+	aspiring to be any kind of political blogger, I can conclude that it has pros and cons. One con is
+	that it has limited opening hours. As a Swede you learn it in and out, and often assumes the
+	availability of alcohol. But some old holidays that no one cares about sometime leads to deviating
+	opening hours that are <strong>really</strong> easy to forget!
+</p>
+
+<p>
+	<i>Is bolaget closed</i> is both a site for easeli browes your favorite stores opening hours (they
+	do vary). But also provides a API for querying if the opening hourse deviates. Put the query on a automation
+	with some notification when deviating, and you will never stay unwantedly sober.
+</p>
+<ul>
+	<li><a href="https://is-bolaget-closed.ricw.se">See it live</a></li>
+	<li><a href="https://github.com/wric/is-bolaget-closed">Source code on Github</a></li>
+</ul>
+
+<style>
+</style>
